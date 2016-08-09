431849f33e8abdbe6ef38722f1aca9c755679923
jorgeperis <jorgeperis@gmail.com>
2016-08-09 15:34:08 +0200

    add byebug to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -23,6 +23,7 @@ class Tag < ApplicationRecord
 
   def try_to_save(userTags,user)
     if self.save
+      byebug
       userTags.push(self)
     else
       existing_tag = Tag.find_by(name: self.name)
